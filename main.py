df6b097b233c4cd2cbded6a72c5e941705b2477a
HaikalAkif <haikalakif17@gmail.com>
2023-04-28 16:01:19 +0800

    Rework

--- main.py
+++ main.py
@@ -4,81 +4,79 @@ from tkinter import *
 from pydub import AudioSegment
 from pydub.playback import play
 from PIL import ImageTk, Image
+from music import MusicUtil
 
-# Define the function to start the game
-def start_game():
-    # Create the game window
-    game_window = tk.Toplevel(window)
-    game_window.title("My Game")
+
+class MainWindow():
+
+    def __init__(self, music_player, window):
+        self.music_player = music_player
+
+        self.window = window
+        self.window.geometry('600x400')
+        self.window.title("Game menu")
+
+        #Set Dimensions
+        self.canvas = tk.Canvas(self.window, width=600, height=400)
+        self.canvas.pack()
+
+        # Create Background image
+        bg_image = tk.PhotoImage(file="eid.png")
+        self.canvas.create_image(0, 0, anchor="nw", image=bg_image)
+
+        frame = tk.Frame()
+        frame.pack()
+
+        # Create a label for the title
+        self.title_label = tk.Label(window, text="iWord Hunt!", font=("Inter", 24))
+        self.title_label.pack(pady=30)
+
+        # Create a button to start the game
+        self.start_button = tk.Button(window, text="Start Game", command=self.start_game, font=("Inter", 16))
+        self.start_button.pack(pady=10)
+
+        # Create a option button
+        self.option_button = tk.Button(window, text="Option", command= self.option_page , font=("Inter", 16))
+        self.option_button.pack(pady=10)
+
+        # Create a button to exit the game
+        self.exit_button = tk.Button(window, text="Exit", command=window.destroy, font=("Inter", 16))
+        self.exit_button.pack(pady=10)
+
+
+    # Define function to start the game
+    def start_game(self):
+        #  Create the game window
+        self.game_window = tk.Toplevel(self.window)
+        self.game_window.title("My Game")
     
-    # Create the game content
-    game_label = tk.Label(game_window, text="iWord Hunt!", font=("Inter", 24), bg="blue", width=400, height=300)
-    game_label.pack(pady=20)
+        # Create the game content
+        self.game_label = tk.Label(self.game_window, text="iWord Hunt!", font=("Inter", 24), bg="blue", width=400, height=300)
+        self.game_label.pack(pady=20)
     
+        # Hide the main menu window
+        self.window.withdraw()
+
+    def option_page(self):
+        # Create option window
+        game_option = tk.Toplevel(self.window)
+        game_option.title("Game Option")
+
+        # Create option content
+        game_option_label = tk.Label(game_option, text="Game Option", font=("Inter", 24), bg="green", fg="black", width=400, height=300)
+        print("1.Language 2.Volume")
+
     # Hide the main menu window
-    window.withdraw()
+    def exit_program(self):   
+        self.window.withdraw()
 
-def option_page():
-    # Create option window
-    game_option = tk.Toplevel(window)
-    game_option.title("Game Option")
+    def run(self):
+        self.window.mainloop()
 
-    # Create option content
-    game_option_label = tk.Label(game_option, text="Game Option", font=("Inter", 24), bg="green", fg="black", width=400, height=300)
-    print("1.Language 2.Volume")
+if __name__ == "__main__":
 
-    # Hide the main menu window
-    window.withdraw()
+    window = tk.Tk()
+    music_util = MusicUtil("sdhr.mp3")
 
-#Load and play song
-# def play_song():
-#     play(song)
-#     window.after(len(song), play_song)
-
-# Create the main window
-window = tk.Tk()
-window.geometry('600x400')
-window.title("Game Menu")
-
-# song = AudioSegment.from_mp3("sdhr.mp3")
-# play_song() 
-# # Set the dimensions of the window
-canvas = tk.Canvas(window, width=600, height=400)
-canvas.pack()
-
-# #  Load the background image
-bg_image = tk.PhotoImage(file = "eid.png")
-
-# # Create the background image
-canvas.create_image(0, 0, anchor="nw", image=bg_image)
-# x jadi
-
-
-
-
-# Create a label for the title
-title_label = tk.Label(window, text="iWord Hunt!", font=("Inter", 24))
-title_label.pack(pady=30)
-
-# Create a button to start the game
-start_button = tk.Button(window, text="Start Game", command=start_game, font=("Inter", 16))
-start_button.pack(pady=10)
-
-# Create a option button
-option_button = tk.Button(window, text="Option", command= option_page , font=("Inter", 16))
-option_button.pack(pady=10)
-
-# Create a button to exit the game
-exit_button = tk.Button(window, text="Exit", command=window.destroy, font=("Inter", 16))
-exit_button.pack(pady=10)
-
-# Start the main event loop
-window.mainloop()
-
-
-
-
-
-
-
-# xjadi
+    w = MainWindow(music_util, window)
+    w.run()
---
+++ music.py
@@ -0,0 +1,20 @@
+#import
+from pydub import AudioSegment
+from pydub.playback import play
+import multiprocessing
+
+class MusicUtil:
+    def init__(self, music_path):
+        self.music_path = music_path
+        self.music_process = None
+
+    def play_music(self):
+        song = AudioSegment.from_mp3(self.music_path)
+        play(song)
+
+    def start_playback(self):
+        self.music_process = multiprocessing.Process(target = self.play_music)
+        self.music_process.start()
+
+    def stop_playback(self):
+        self.music_process.kill()
